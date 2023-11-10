49e4e8f7aecd59bec88d49e8edc40fe6ddcadfec
Velaron <mars873@gmail.com>
2023-11-10 12:08:14 +0000

    add ppc64 support

--- regamedll/engine/archtypes.h
+++ regamedll/engine/archtypes.h
@@ -27,7 +27,7 @@
 */#ifndef ARCHTYPES_H
 #define ARCHTYPES_H
 
-#if defined(__x86_64__) || defined(_WIN64) || defined(__aarch64__)
+#if defined(__x86_64__) || defined(_WIN64) || defined(__aarch64__) || defined(__powerpc64__) || defined(__ppc64__)
 #define X64BITS
 #endif
 
